c969ad1e82d7a87850ed75625983972aaf5c6d56
Daniel Wheeler <daniel.wheeler2@gmail.com>
2019-03-07 13:32:37 -0500

    finished app

--- app.py
+++ app.py
@@ -3,16 +3,14 @@ from starlette.responses import JSONResponse, HTMLResponse
 import uvicorn
 import aiohttp
 import asyncio
-from fastai.vision import open_image
+from fastai.vision import open_image, load_learner, defaults, torch, Path
 from io import BytesIO
 
 
+defaults.device = torch.device('cpu')
+
 app = Starlette()
 
-
-#@app.route('/')
-#async def homepage(request):
-#    return JSONResponse({'hello': 'world'})
 
 async def get_bytes(url):
     async with aiohttp.ClientSession() as session:
@@ -22,16 +20,9 @@ async def get_bytes(url):
     
 def predict_image_from_bytes(bytes):
     img = open_image(BytesIO(bytes))
-    print(type(img))
-    return JSONResponse({"something":"blah"})
-    #losses = img.predict(cat_learner)
-    #return JSONResponse({
-    #    "predictions": sorted(
-    #        zip(cat_learner.data.classes, map(float, losses)),
-    #        key=lambda p: p[1],
-    #        reverse=True
-    #    )
-    #})
+    learn = load_learner(Path('data/plants'))
+    pred_class, _, _ = learn.predict(img)
+    return JSONResponse({"prediction": pred_class.obj})
             
     
 @app.route("/classify-url", methods=["GET"])
@@ -44,30 +35,13 @@ async def classify_url(request):
 def form(request):
     return HTMLResponse(
         """
-        <form action="/upload" method="post" enctype="multipart/form-data">
-            Select image to upload:
-            <input type="file" name="file">
-            <input type="submit" value="Upload Image">
-        </form>
-        Or submit a URL:
+        Submit an image of either an Aloe, an Anthurium or an Arrowhead plant for classification.
         <form action="/classify-url" method="get">
             <input type="url" name="url">
             <input type="submit" value="Fetch and analyze image">
         </form>
     """)
     
-@app.route("/classify-url", methods=["GET"])
-async def classify_url(request):
-    bytes = await get_bytes(request.query_params["url"])
-    img = open_image(BytesIO(bytes))
-    _,_,losses = learner.predict(img)
-    return JSONResponse({
-        "predictions": sorted(
-            zip(cat_learner.data.classes, map(float, losses)),
-            key=lambda p: p[1],
-            reverse=True
-        )
-    })
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=5000)
